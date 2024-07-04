6b4e2acc3f0673dd19ff16917de80b85642a2176
Aleksandr Dekunov <wresda@gmail.com>
2024-07-04 17:41:24 +0300

    4

--- main registration.py
+++ main registration.py
@@ -7,9 +7,10 @@ def main(page: ft.Page):
     page.window_width = 350
     page.window_height = 400
     page.window_resizable = False
-    
 
+    user_login = ft.TextField(label='Логин', width=200)
+    user_pass = ft.TextField(label='Пароль', width=200)
+    btn_reg = ft.OutlinedButton(text='Добавить', width=200, on_click=register, disabled=True)
 
 
 ft.app(target=main)
-
